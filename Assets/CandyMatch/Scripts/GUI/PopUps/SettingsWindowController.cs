812fb53a49245532a4ab8d88740891869d4b9781
PavloMarkovskiy <pavlo.markovskiy@midnight.works>
2025-03-06 10:15:19 +0000

    ui fix

--- Assets/CandyMatch/Scripts/GUI/PopUps/SettingsWindowController.cs
+++ Assets/CandyMatch/Scripts/GUI/PopUps/SettingsWindowController.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace Mkey {
     public class SettingsWindowController : PopUpsController
     {
+        [SerializeField] private GameObject _gameView;
+        [SerializeField] private GameObject _homeView;
         [SerializeField]
         private Toggle easyToggle;
         [SerializeField]
@@ -14,7 +17,17 @@ namespace Mkey {
         #region regular
         private void Start()
         {
-           if(easyToggle) easyToggle.onValueChanged.AddListener((value) =>
+            if (SceneManager.GetActiveScene().name == "1_Map")
+            {
+                _gameView.SetActive(false);
+                _homeView.SetActive(true);
+            }
+            else if (SceneManager.GetActiveScene().name == "2_Game")
+            {
+                _gameView.SetActive(true);
+                _homeView.SetActive(false);
+            }
+            if (easyToggle) easyToggle.onValueChanged.AddListener((value) =>
             {
                 MSound.SoundPlayClick(0, null);
                 if (value) { HardModeHolder.Instance.SetMode(HardMode.Easy); }
@@ -24,6 +37,11 @@ namespace Mkey {
             RefreshWindow();
         }
         #endregion regular
+
+        public void GoHome()
+        {
+            SceneManager.LoadScene("1_Map");
+        }
 
         public override void RefreshWindow()
         {
